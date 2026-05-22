9f4a1ba55e5e08f9372f072bd5c20b904b132382
Archan Ghosh <gharchan@gmail.com>
2026-05-22 21:37:57 +0530

    update dataloader

--- src/stl10_loader.py
+++ src/stl10_loader.py
@@ -2,19 +2,26 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 
 from src.config import DEVICE
 
 
-def get_stl10_dataloader(batch_size=32, image_size=128, split="train", data_root="./data"):
+def get_stl10_dataloader(batch_size=32, image_size=128, target_class = 0, split="train", data_root="./data"):
     transform = transforms.Compose([
         transforms.Resize((image_size, image_size)),
         transforms.ToTensor(),
     ])
     dataset = datasets.STL10(root=data_root, split=split, download=True, transform=transform)
     dataset.data = dataset.data[:5000]
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+    subset_indices = [i for i, (_, label) in enumerate(dataloader.dataset) if label == target_class]
+
+    dataloader = DataLoader(Subset(dataset, subset_indices),
+                            batch_size = batch_size,
+                            shuffle=True,
+                            drop_last=True)
+    
     return dataloader
 
 #loader = get_stl10_dataloader(batch_size=4, image_size=128)
