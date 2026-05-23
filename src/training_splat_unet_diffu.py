f487d5001a243a5a85a794b4d2fdf48cbde9756b
Archan Ghosh <gharchan@gmail.com>
2026-05-23 10:49:22 +0530

    updates to training pipeline

--- src/training_splat_unet_diffu.py
+++ src/training_splat_unet_diffu.py
@@ -1,5 +1,7 @@
 import os
 import gc
+import argparse
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -14,29 +16,56 @@ from src.utils.create_data_latents import create_latents
 from src.utils.generate_samples import generate_samples_hq
 
 
-from src.config import DEVICE, BASE_CHKPNT_DIR, SPLAT_ENCODER_SAVE_NAME, SPLAT_RENDERER_SAVE_NAME, IMG_SIZE, SPLAT_ENCODER_BASE_BATCH_SIZE, TARGET_CLASS
+from src.config import DEVICE, BASE_CHKPNT_DIR, SPLAT_ENCODER_SAVE_NAME, SPLAT_RENDERER_SAVE_NAME, IMG_SIZE, SPLAT_ENCODER_BASE_BATCH_SIZE, TARGET_CLASS, LONG_RUN_EPOCHS, LOG_INTERVAL, SAVE_INTERVAL
 
-try:
-    encoder = load_encoder_state()
-    if encoder is None:
-        raise ValueError("Encoder Failed to Load")
+def run_splat_diff_training(run_flag, start_long_epochs, end_long_epochs, save_intr, log_intr):
+    try:
+        if run_flag not in ["start", "resume"]:
+            raise ValueError("Run Flag is incorrect, should be 'start' or'resume' ")
+        if run_flag == 'start':
+
+
+        elif run_flag == 'resume':
+
+            encoder = load_encoder_state()
+            if encoder is None:
+                raise ValueError("Encoder Failed to Load")
+            
+            renderer = load_renderer_state()
+            if renderer is None:
+                raise ValueError("Renderer Failed to Load")
+        
+
+        train_data = get_stl10_dataloader(batch_size=SPLAT_ENCODER_BASE_BATCH_SIZE, image_size=IMG_SIZE, target_class=TARGET_CLASS)
+        
+        GLOBAL_MIN, GLOBAL_MAX = create_latents(encoder, train_data)
+
+        if GLOBAL_MIN is None or GLOBAL_MAX is None:
+            raise ValueError("Couldn't Calculate GLOBAL_MIN or GLOBAL_MAX")
+        
+
+
+
+        
+
+
+    except Exception as e:
+        print(f"\n{'-'*10} Error Occured during Training of GS Diffusion : {e} {'-'*10}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
     
-    renderer = load_renderer_state()
-    if renderer is None:
-        raise ValueError("Renderer Failed to Load")
-    
+    parser.add_argument("--run_flag", choices=["start", "resume"])
 
-    train_data = get_stl10_dataloader(batch_size=SPLAT_ENCODER_BASE_BATCH_SIZE, image_size=IMG_SIZE, target_class=TARGET_CLASS)
-    
-    GLOBAL_MIN, GLOBAL_MAX = create_latents(encoder, train_data)
+    parser.add_argument("--start_long_run_epochs", type=int)
 
-    if GLOBAL_MIN is None or GLOBAL_MAX is None:
-        raise ValueError("Couldn't Calculate GLOBAL_MIN or GLOBAL_MAX")
-    
+    parser.add_argument("--end_long_run_epochs", type=int)
 
+    parser.add_argument("--save_interval", type=int)
 
-    
+    parser.add_argument("--log_interval", type=int)
 
+    args = parser.parse_args()
 
-except Exception as e:
-    print(f"\n{'-'*10} Error Occured during Training of GS Diffusion : {e} {'-'*10}")
+    run_splat_diff_training(args.run_flag, args.start_long_run_epochs, args.end_long_run_epochs, args.save_interval, args.log_interval)
